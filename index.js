ae433e4b92ff8405d00bd5d8125dccd20d15a302
Mostafa-Magdy-7 <Eng.mostafaalattar@gmail.com>
2025-08-28 09:05:01 +0300

    Updated index.js file

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import path from "path";
 
 const app = express();
 const port = 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
